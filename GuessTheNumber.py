59d712ef249c3f33942113d03ff42cef960aa387
Chaos <106764908+Chaos0101@users.noreply.github.com>
2022-07-08 23:43:35 +0300

    GuessTheNumGame
    Python Game

---
+++ GuessTheNumber.py
@@ -0,0 +1,37 @@
+import time
+from random import seed
+from random import randint
+
+seed(1)
+for _ in range(10):
+    value = randint(1, 100000)
+def guess123():
+      print ('''
+Type (exit) To Exit!
+Type (num) To See The Number!
+                                                   ''')
+      x = input('Enter a number from 1 until 100000: ')
+      x=int(x)
+      if x == value:
+          print ('Congratulations You Find It!')
+          print (value)
+          time.sleep(3)
+          z = input('Do you want to play again [y/n]: ')
+          if z == 'y':
+               guess123()
+          elif z == 'n':
+               exit()
+          else:
+               print ('Wrong Option!')
+               time.sleep(2)
+               exit() 
+
+      elif x < value:
+          print ("Higher (+)")
+          guess123()
+
+      elif x > value:
+          print ("Lower (-)")
+          guess123()
+
+guess123()
